da0cfcba3c7a95a614f845a3a2049b4bdb4f1a69
Angelina <lina.khrennikova@mail.ru>
2024-09-29 17:20:07 +0300

    using json

--- Grabfri/main.py
+++ Grabfri/main.py
@@ -1,8 +1,11 @@
+
 import time
 import vk_api
 import matplotlib.pyplot as plt
 from matplotlib import pylab
 import networkx as nx
+import json
+from collections import defaultdict
 
 import util
 from util import *
@@ -40,9 +43,52 @@ def save_graph(graph, file_name):
     pylab.close()
     del fig
 
+def json_to_dict_of_lists(file_path):
+    # Открываем JSON файл и считываем данные
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Создаем словарь, где ключи - это user_id, а значения - списки множеств друзей
+    result = {}
+    all_friends = []
+
+    for entry in data:
+        friend_info = User({
+            "id": entry['friend_id'],
+            "first_name": entry['first_name'],
+            "last_name": entry['last_name'],
+            "domain": 'id' + str(entry['friend_id'])
+        })
+
+        all_friends.append(friend_info)
+
+    
+    for entry in data:
+        user_info = {
+            "id": 1,
+            "first_name": "Иван",
+            "last_name": "Иванов",
+            "is_closed": False,
+            "domain": "id"
+        }
+
+        # Создание объекта User
+        tmp = User(user_info)
+        for friend in all_friends:
+            if entry['user_id'] == friend.id:
+                tmp = friend
+        tmp_friends = []
+        for entry in data:
+            for friend in all_friends:
+                if entry['friend_id'] == friend.id:
+                    tmp_friends.append(friend)
+        result[tmp] = list(set(tmp_friends))
+
+    return result
+
 
 def main():
-    base_users = read_ids_from_file('user_ids.txt')
+    """ base_users = read_ids_from_file('user_ids.txt')
     print("Base users ids : ", base_users)
 
     auth_token = util.parse_token('token.txt')
@@ -76,7 +122,12 @@ def main():
             except:
                 print("User banned")
         else:
-            graph[friend] = list()
+            graph[friend] = list() 
+    
+    #graph = json_to_dict_of_lists('friends.json')
+    print(graph) """
+
+    graph = json_to_dict_of_lists('friends_1.json')
 
     # here all collected data should be written for further operations
     nxgraph = nx.from_dict_of_lists(graph)
