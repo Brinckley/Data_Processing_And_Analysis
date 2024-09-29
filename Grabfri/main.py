65d1be7125c8e17833ec16408a886555a11f7fad
Aleksandr Savin <57827932+Brinckley@users.noreply.github.com>
2024-09-29 13:29:30 +0300

    drawer optimized

--- Grabfri/main.py
+++ Grabfri/main.py
@@ -20,27 +20,6 @@ def get_friends(vk, user_id):
     return lst
 
 
-def save_graph(graph, file_name):
-    plt.figure(num=None, figsize=(120, 120), dpi=80)
-    plt.axis('off')
-    fig = plt.figure(1)
-    pos = nx.spring_layout(graph)
-    nx.draw_networkx_nodes(graph, pos)
-    nx.draw_networkx_edges(graph, pos)
-    nx.draw_networkx_labels(graph, pos)
-
-    cut_x = 1.50
-    cut_y = 1.50
-    xmax = cut_x * max(xx for xx, yy in pos.values())
-    ymax = cut_y * max(yy for xx, yy in pos.values())
-    plt.xlim(-xmax, xmax)
-    plt.ylim(-ymax, ymax)
-
-    plt.savefig(file_name, bbox_inches="tight")
-    pylab.close()
-    del fig
-
-
 def main():
     base_users = read_ids_from_file('user_ids.txt')
     print("Base users ids : ", base_users)
@@ -82,23 +61,23 @@ def main():
     nxgraph = nx.from_dict_of_lists(graph)
     #
     # drawing basic graph
-    drawer.draw_graph(nxgraph, 'test_graph.html')
+    drawer.draw_graph(nxgraph, 'test_graph1.html')
     #
     # calculating centers
     eigenvector_res_10 = algo.calc_by_eigenvector(nxgraph, 5)
     for eres in eigenvector_res_10:
         print("User name {} with value {}".format(eres[0], eres[1]))
-    drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'eigenvector_highlighted.html')
+    drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'eigenvector_highlighted1.html')
 
     closeness_res_10 = algo.calc_by_closeness(nxgraph, 5)
     for cres in closeness_res_10:
         print("User name {} {} ".format(cres[0].first_name, cres[0].last_name))
-    drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'closeness_highlighted.html')
+    drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'closeness_highlighted1.html')
 
     betweenness_res_10 = algo.calc_by_betweenness(nxgraph, 5)
     for bres in betweenness_res_10:
         print("User name {} {} ".format(bres[0].first_name, bres[0].last_name))
-    drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'betweenness_highlighted.html')
+    drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'betweenness_highlighted1.html')
 
 
 
