b2e910b0d580c8f29585d8198a4f37151d035d12
Aleksandr Savin <57827932+Brinckley@users.noreply.github.com>
2024-09-29 17:40:05 +0300

    several updates

--- Grabfri/main.py
+++ Grabfri/main.py
@@ -23,59 +23,14 @@ def get_friends(vk, user_id):
     return lst
 
   
-def json_to_dict_of_lists(file_path):
-    # Открываем JSON файл и считываем данные
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    # Создаем словарь, где ключи - это user_id, а значения - списки множеств друзей
-    result = {}
-    all_friends = []
-
-    for entry in data:
-        friend_info = User({
-            "id": entry['friend_id'],
-            "first_name": entry['first_name'],
-            "last_name": entry['last_name'],
-            "domain": 'id' + str(entry['friend_id'])
-        })
-
-        all_friends.append(friend_info)
-
-    
-    for entry in data:
-        user_info = {
-            "id": 1,
-            "first_name": "Иван",
-            "last_name": "Иванов",
-            "is_closed": False,
-            "domain": "id"
-        }
-
-        # Создание объекта User
-        tmp = User(user_info)
-        for friend in all_friends:
-            if entry['user_id'] == friend.id:
-                tmp = friend
-        tmp_friends = []
-        for entry in data:
-            for friend in all_friends:
-                if entry['friend_id'] == friend.id:
-                    tmp_friends.append(friend)
-        result[tmp] = list(set(tmp_friends))
-
-    return result
-
-  
 def main():
-    graph = json_to_dict_of_lists('friends_1.json')
-
+    graph = json_to_dict_of_lists('friends_1.json') # small json file here
     # here all collected data should be written for further operations
     nxgraph = nx.from_dict_of_lists(graph)
-    #
+
     # drawing basic graph
     drawer.draw_graph(nxgraph, 'test_graph1.html')
-    #
+
     # calculating centers
     eigenvector_res_10 = algo.calc_by_eigenvector(nxgraph, 5)
     for eres in eigenvector_res_10:
@@ -91,7 +46,6 @@ def main():
     for bres in betweenness_res_10:
         print("User name {} {} ".format(bres[0].first_name, bres[0].last_name))
     drawer.draw_graph_highlighted(nxgraph, eigenvector_res_10, 'betweenness_highlighted1.html')
-
 
 
 if __name__ == '__main__':
--- Grabfri/util.py
+++ Grabfri/util.py
@@ -1,3 +1,7 @@
+import json
+from user import User
+
+
 def read_ids_from_file(file_name):
     user_file = open(file_name, 'r')
     count = 0
@@ -16,3 +20,43 @@ def read_ids_from_file(file_name):
 def parse_token(token_file_name):
     toke_file = open(token_file_name, 'r')
     return toke_file.readline()
+
+
+def json_to_dict_of_lists(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    result = {}
+    all_friends = []
+
+    for entry in data:
+        friend_info = User({
+            "id": entry['friend_id'],
+            "first_name": entry['first_name'],
+            "last_name": entry['last_name'],
+            "domain": 'id' + str(entry['friend_id'])
+        })
+
+        all_friends.append(friend_info)
+
+    for entry in data:
+        user_info = {
+            "id": 1,
+            "first_name": "Иван",
+            "last_name": "Иванов",
+            "is_closed": False,
+            "domain": "id"
+        }
+
+        tmp = User(user_info)
+        for friend in all_friends:
+            if entry['user_id'] == friend.id:
+                tmp = friend
+        tmp_friends = []
+        for entry in data:
+            for friend in all_friends:
+                if entry['friend_id'] == friend.id:
+                    tmp_friends.append(friend)
+        result[tmp] = list(set(tmp_friends))
+
+    return result
